cc98a50d7b030439ee7615e3ae49a4545f641cb9
younishamrh10 <yoha06001@utb.vaxjo.se>
2024-04-05 13:11:59 +0200

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inlämninguppgift._1_oop
+{
+    internal class bil
+    {
+        private string märke;
+        private string modell;
+        private string färg;
+        private int ålder;
+        private int mil;
+
+        public bil(string märke2, string modell2, string färg2, int ålder2, int mil2)
+        {
+            märke = märke2;
+            modell = modell2;
+            färg = färg2;
+            ålder = ålder2;
+            mil = mil2;
+        }
+        public void Mymetod()
+        {
+            Console.WriteLine(märke);
+            Console.WriteLine(modell);
+            Console.WriteLine(färg);
+            Console.WriteLine(ålder);
+            Console.WriteLine(mil);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Inlämninguppgift._1_oop;
+using System;
+
+namespace Inlämninguppgift._1_oop
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            bil p = new bil("volvo", "x70", "vit", 1, 35000);
+
+            bil p2 = new bil("BMW", "m5", "svart", 1, 10000);
+
+            p.Mymetod();
+            Console.WriteLine("");
+            p2.Mymetod();
+        }
+    }
+}
